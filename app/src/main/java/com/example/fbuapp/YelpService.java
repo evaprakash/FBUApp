c6ac878b46b8e4a91a55932ad6dabc625c71bf61
Eva Isha Prakash <eprakash@fb.com>
2021-07-22 16:16:01 -0700

    Deleted extra comment

--- app/src/main/java/com/example/fbuapp/YelpService.java
+++ app/src/main/java/com/example/fbuapp/YelpService.java
@@ -12,7 +12,6 @@ import retrofit2.http.Query;
 
 
 public interface YelpService {
-    //static final String API_KEY = "";
     @Headers({"Authorization: Bearer LNaxxtZQkOaK1kgcOt5GWyR03XLK9f845_ERPH-OkWHT0uid2MGrwgbkwRjSljBwbiqrCIgQ_HmOe_9AmXbaEmZphE0iuub830kk4yuqn4HX7cQZ8JKJH_1T-N34YHYx"})
     @GET("businesses/search")
     Call<ResponseBody> filteredSearch(@Query("term") String term, @Query("location") String location);
